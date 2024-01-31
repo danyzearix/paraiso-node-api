530e198c32d01d8ddeced08327ee8a1f12f85827
danydev <atriasoft.ceo@gmail.com>
2024-01-30 22:31:37 -0500

    Fecha mongo modificada

--- models/obituariosSchema.js
+++ models/obituariosSchema.js
@@ -7,10 +7,22 @@ const obituariosSchema = new mongoose.Schema({
     exequias: String,
     hora: String,
     destino_final: String,
-    fecha: Date,
-  });
-  
-  // Crear el modelo basado en el esquema
-  const Obituarios = mongoose.model('Obituarios', obituariosSchema);
+    fecha: String, // Cambiado a String
+});
 
-  export default Obituarios
+// Función para formatear la fecha como DD/MM/AAAA
+const formatearFechaBogota = (fecha) => {
+    const opcionesFecha = { timeZone: 'America/Bogota', day: '2-digit', month: '2-digit', year: 'numeric' };
+    return new Intl.DateTimeFormat('es-CO', opcionesFecha).format(fecha);
+};
+
+// Middleware para formatear la fecha antes de guardar
+obituariosSchema.pre('save', function (next) {
+    this.fecha = formatearFechaBogota(this.fecha);
+    next();
+});
+
+// Crear el modelo basado en el esquema
+const Obituarios = mongoose.model('Obituarios', obituariosSchema);
+
+export default Obituarios;
